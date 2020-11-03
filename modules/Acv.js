47331c230c034ce3342c139e9e70f868f602b3ea
NerdioN <neel99khalade@gmail.com>
2020-11-03 21:15:50 +0530

    auto scroll & link generator

--- modules/Acv.js
+++ modules/Acv.js
@@ -1,22 +1,78 @@
 const puppeteer = require('puppeteer')
 const PUPPETEER_UI_FLAG = false
-
+const mongo = require('./DB')
 class Acv {
     constructor() {
         this.page;
         this.checkLoggedInSelector = '#breadcrumbs'
     }
+
+    async autoScroll(){
+        await this.page.evaluate(async () => {
+            await new Promise((resolve, reject) => {
+                var totalHeight = 0;
+                var distance = 200;
+                var scrollHeight = 5000;
+                var timer = setInterval(() => {
+                    window.scrollBy(0, distance);
+                    totalHeight += distance;
+    
+                    if(totalHeight >= scrollHeight){
+                        clearInterval(timer);
+                        resolve();
+                    }
+                }, 500);
+            });
+        });
+    }
+
+
     async startScraping() {
+        const auctionsSelector = ''
         try {
             let launchBrowser = await this.launchBrowser()
-            console.log('launch',launchBrowser)
-            if (launchBrowser) {
-                let getLoggedIn = await this.getLoggedIn()
+            let getLoggedIn = await this.getLoggedIn()
+            if (launchBrowser && getLoggedIn) {
+               console.log('finished logging in')
+               await this.page.click('.header-link.ended-auctions a')
             }
-            
-        } catch (err) {
 
+            await this.page.waitForTimeout(2000)
+            let countIndex = 0
+            while(1) {
+                // console.log(countIndex, ' is my count')
+                countIndex = await this.getPostDetails(countIndex)
+                await this.autoScroll()
+            }
+        } catch (err) {
+            return { sucess: false, message: 'Unable to process'}
         }
+    }
+
+    async getPostDetails(postCounter) {
+        let findCount = await this.page.$$(`.container .has-filters-bar [id^="auction-"]`)
+        console.log(findCount.length, 'length')
+        let postsCount = findCount.length
+        let i
+        for(i=postCounter; i< postsCount; i++) {
+            let auction = await this.page.$(`.container .has-filters-bar [id^="auction-"]:nth-child(${i})`)
+            if(auction) {
+                let vehicleIsSold = await auction.$eval('.auction-card__content > a > .action-bar > .button-container button', el => el.textContent)
+                if(vehicleIsSold == " VEHICLE SOLD ") {
+                    let postLink = await auction.$eval('a', el => el.href)
+                    let ifDataFound = await mongo.usaacv.collection('acvLink').findOne({ href: postLink})
+                    if(!ifDataFound) {
+                        await mongo.usaacv.collection('acvLinks').insertOne({href : postLink})
+                        console.log('adding-', postLink)
+                    } else {
+                        console.log('already inserted', postLink)
+                    }
+                } else {
+                    console.log('!sold')
+                }
+            }
+        }
+        return i
     }
 
     async launchBrowser() {
@@ -25,10 +81,9 @@ class Acv {
                 headless: PUPPETEER_UI_FLAG,
             })
             const context = await browser.createIncognitoBrowserContext()
-            let page = await context.newPage()
+            this.page = await context.newPage()
 
-            await page.setViewport({ width: 1366, height: 768 })
-            this.page = page
+            await this.page.setViewport({ width: 1366, height: 760 })
             return true
         } catch (err) {
             return false
@@ -40,7 +95,6 @@ class Acv {
 
     async getLoggedIn() {
         try {
-            const loginSelector = '#login_button'
             const emailSelector = '#login_email'
             const passSelector = '#login_password'
             const loginButtonSelector = '#login_button'
@@ -48,165 +102,18 @@ class Acv {
             const pass = 'Password231$'
 
             await this.page.goto('https://app.acvauctions.com/login')
-            console.log('i am here 1')
             //getting logged in
-            await this.page.waitForSelector(loginSelector)
-            //await this.page.click(loginSelector)
-
-            await this.page.waitForSelector(emailSelector)
+            await this.page.waitForSelector(loginButtonSelector)
             await this.page.type(emailSelector, email)
             await this.page.type(passSelector, pass)
             await this.page.click(loginButtonSelector)
             await this.page.waitForSelector(this.checkLoggedInSelector)
-            console.log('i am here 2')
             return true
         } catch (err) {
             return false
         }
 
     }
-
-    // async getDetails() {
-    //     const propertySelector = {
-    //         price : '.classified__price .sr-only',
-    //         address : '.classified__information--address-clickable',
-    //         agencyName:'#customer-card > div.customer-card__body > div',
-    //         agencyLogo:'#customer-card > div.customer-card__body > div img'
-    //     }
-
-    //     // const propertySelectorRegex = {
-    //     //     propertyType = /(?<=classified\/).*(?=\/for-sale)/
-    //     // }
-
-
-    //     await db.start()
-
-    //     let sourceUrl = 'https://www.immoweb.be/en/classified/house/for-sale/woluwe-saint-pierre/1150/8856902?searchId=5f758f8b89133and/for-sale/mouscron/7700/8566383'
-    //     await this.getLoggedIn()
-    //     await this.page.goto(sourceUrl,{waitUntil: "networkidle2"})
-
-    //     await this.page.waitForTimeout(500)
-
-    //     let propertyDetails = {
-    //         price: null,
-    //         address: null,
-    //         category: null,
-    //         livingSurface: null,
-    //         outdoorSurface: null,
-    //         noOfBedrooms: null,
-    //         noOfBathrooms: null,
-    //         noOfToilets: null,
-    //         energyClass: null,
-    //         yearBuilt: null,
-    //         buildingConditions: null,
-
-    //         agencyLogo:null,
-    //         agencyName: null,
-    //         agencyAddress:null,
-    //         agencyContact:null,
-    //         agencyWebsite:null,
-
-    //         postedOn:null,
-    //         views:null,
-    //         saves:null
-    //     }
-
-    //     let regexPatterns = {
-    //         category : new RegExp('(?<=classified\/).*(?=\/for-sale)',''),
-    //         outdoorSurface : ''
-    //     }
-
-    //     let sData = await this.page.evaluate(()=>{
-    //         let statData = document.querySelector("#main-content > div:nth-child(2) > div > div > div.classified__section--statistic > p").innerText
-    //         return statData
-    //     })
-    //     //console.log(sData)
-    //     sData = sData.split('|')
-    //     let statStr=''
-    //     for(let i=0;i<sData.length;i++){
-    //         statStr += sData[i]+'\n'
-    //     }
-    //     console.log(statStr)
-    //     if(await this.page.$(propertySelector.price)) {
-    //         propertyDetails.price = await this.page.$eval(propertySelector.price, e=> e.innerText)
-    //     }
-
-    //     if(await this.page.$(propertySelector.address)) {
-    //         propertyDetails.address = await this.page.$eval(propertySelector.address, e=> e.innerText)
-    //     }
-    //     if(await this.page.$(propertySelector.agencyName)) {
-    //         propertyDetails.agencyName = await this.page.$eval(propertySelector.agencyName, e=> e.innerText)
-    //     }
-    //     if(await this.page.$(propertySelector.agencyLogo)) {
-    //         propertyDetails.agencyLogo = await this.page.$eval(propertySelector.agencyLogo, e=> e.src)
-    //     }
-
-    //     propertyDetails.category = await this.page.url().match(regexPatterns.category)[0]
-
-
-    //     let textualData = await this.page.evaluate(()=>{
-    //         let dataInText = ''
-    //         let data = document.querySelectorAll('.classified-table__row')
-
-    //         for(let i=0; i<data.length; i++) {
-    //             dataInText += JSON.stringify(data[i].innerText) + '\n'
-    //         }
-    //         return dataInText
-    //     })
-    //     //exterior surface
-    //     //if(textualData.match(regexPatterns.outdoorSurface)) propertyDetails.outdoorSurface = await textualData.match(regexPatterns.outdoorSurface)[0]
-    //     //console.log(textualData)
-    //     let m
-    //     if(m = await textualData.match(/(?<=Surface of the plot\\t)\d+/gm)) {
-    //         propertyDetails.outdoorSurface = m[0]
-    //     }
-    //     if(m = await textualData.match(/(?<=Bedrooms\\t)\d+/gm)) {
-    //         propertyDetails.noOfBedrooms = m[0]
-    //     }
-    //     if(m = await textualData.match(/(?<=Bathrooms\\t)\d+/gm)) {
-    //         propertyDetails.noOfBathrooms = m[0]
-    //     }
-    //     if(m = await textualData.match(/(?<=Toilets\\t)\d+/gm)) {
-    //         propertyDetails.noOfToilets = m[0]
-    //     }
-    //     if(m = await textualData.match(/(?<=Living area\\t)\d+/gm)) {
-    //         propertyDetails.livingSurface = m[0]
-    //     }
-    //     if(m = await textualData.match(/(?<=Energy class\\t)\w+/gm)) {
-    //         propertyDetails.energyClass = m[0]
-    //     }
-    //     if(m = await textualData.match(/(?<=Construction year\\t)\d+/gm)) {
-    //         propertyDetails.yearBuilt = m[0]
-    //     }
-    //     if(m = await textualData.match(/(?<=Building condition\\t).+(?=")/gm)) {
-    //         propertyDetails.buildingConditions = m[0]
-    //     }
-    //     if(m = await textualData.match(/(?<=Website\\t).+(?=")/gm)) {
-    //         propertyDetails.agencyWebsite = m[0]
-    //     }
-    //     if(m = await textualData.match(/(?<=Address\\t).+(?=")/gm)) {
-    //         propertyDetails.agencyAddress = m[0]
-    //     }
-    //     if(m = await statStr.match(/(?<=Posted the ).+/gm)) {
-    //         propertyDetails.postedOn = m[0]
-    //     }
-    //     if(m = await statStr.match(/(?<=Views: )\d+/gm)) {
-    //         propertyDetails.views = m[0]
-    //     }
-    //     if(m = await statStr.match(/(?<=Saves: )\d+/gm)) {
-    //         propertyDetails.saves = m[0]
-    //     }
-
-
-
-
-
-    //     console.log(await propertyDetails)
-    // }
-
-    // async getAuctionedIds(){
-
-    // }
 
 }
 
