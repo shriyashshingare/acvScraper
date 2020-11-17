7619bb06c961032ee033fd520ffaa00546c1d5c1
shriyashshingare <shriyashshingare@gmail.com>
2020-11-17 08:30:07 +0530

    scraper

---
+++ exel.js
@@ -0,0 +1,9 @@
+const Acv = require('./modules/Acv')
+const file = require('./modules/File')
+const mongo = require('./modules/DB')
+const main = async () => {
+    let data = await new file().generateFile()
+}
+main().then(console.log(''))
+// document.querySelector("#auction-detail > div.margin-bottom.container > div:nth-child(3) > div > div")  
+// table table-striped
--- scrapper1.js
+++ scrapper1.js
@@ -2,8 +2,7 @@ const Acv = require('./modules/Acv')
 const file = require('./modules/File')
 const mongo = require('./modules/DB')
 const main = async () => {
-    //var response =  new Acv().startScrapingInsideLink();
-    let data = await new file().generateFile()
+    var response =  new Acv().startScrapingInsideLink();
 }
 main().then(console.log(''))
 // document.querySelector("#auction-detail > div.margin-bottom.container > div:nth-child(3) > div > div")  
